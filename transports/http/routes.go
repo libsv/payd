c07a4badf32e16666dc721ec63ba2d97fbd0cf17
Mark Smith <mark@theflyingcodr.dev>
2021-03-17 11:25:18 +0000

    adding a few potential new endpoint routes

--- transports/http/routes.go
+++ transports/http/routes.go
@@ -3,4 +3,9 @@ package http
 const (
 	RoutePaymentRequest = "r/:paymentID"
 	RoutePayment        = "payment/:paymentID"
+
+	RouteInvoice  = "v1/invoices/:paymentID"
+	RouteInvoices = "v1/invoices"
+
+	RouteBalance = "v1/balance"
 )
