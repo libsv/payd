dbb3999d46521dfaede407a16cd92b763262944f
Tíghearnán Carroll <t.carroll@nchain.com>
2021-09-29 10:49:45 +0100

    skip non api endpoints

--- transports/http/middleware/debug_logger.go
+++ transports/http/middleware/debug_logger.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"strings"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
@@ -9,7 +11,7 @@ import (
 func DebugDumpBody(level log.Lvl) echo.MiddlewareFunc {
 	return middleware.BodyDumpWithConfig(middleware.BodyDumpConfig{
 		Skipper: func(c echo.Context) bool {
-			return log.Level() > level
+			return log.Level() > level || !strings.HasPrefix(c.Path(), "/api/v1/")
 		},
 		Handler: func(e echo.Context, reqBody, respBody []byte) {
 			log.Debug(e.Request().URL.String())
