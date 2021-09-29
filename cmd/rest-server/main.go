7bd31f7c91496ba645ea5228b373b8fa4370b470
Tíghearnán Carroll <tighearnan.carroll@gmail.com>
2021-09-29 11:04:46 +0100

    Debug Middleware (#46)
    * added debug middleware
    
    * skip non api endpoints
    
    * commented func

--- cmd/rest-server/main.go
+++ cmd/rest-server/main.go
@@ -93,6 +93,7 @@ func main() {
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 	e.Use(middleware.RequestID())
+	e.Use(paydMiddleware.DebugDumpBody(log.DEBUG))
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
