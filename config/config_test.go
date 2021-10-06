38620204ebe735b4478bc2f9119708149c969493
Tíghearnán Carroll <t.carroll@nchain.com>
2021-10-06 11:37:34 +0100

    added config validation tests

--- config/config_test.go
+++ config/config_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_ConfigValidate(t *testing.T) {
+func Test_ConfigValidateDB(t *testing.T) {
 	t.Parallel()
 	tests := map[string]struct {
 		cfg *Config
@@ -41,6 +41,69 @@ func Test_ConfigValidate(t *testing.T) {
 				},
 			},
 			err: errors.New("[db.type: value mydb failed to meet requirements]"),
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			err := test.cfg.Validate()
+			if test.err == nil {
+				assert.NoError(t, err)
+				return
+			}
+			assert.EqualError(t, err, test.err.Error())
+
+		})
+	}
+}
+
+func Test_ConfigValidateNetwork(t *testing.T) {
+	t.Parallel()
+	tests := map[string]struct {
+		cfg *Config
+		err error
+	}{
+		"valid deployment network config (mainnet) should return no errors": {
+			cfg: &Config{
+				Deployment: &Deployment{
+					Network: NetworkMainet,
+				},
+			},
+			err: nil,
+		}, "valid deployment network config (testnet) should return no errors": {
+			cfg: &Config{
+				Deployment: &Deployment{
+					Network: NetworkTestnet,
+				},
+			},
+			err: nil,
+		}, "valid deployment network config (stn) should return no errors": {
+			cfg: &Config{
+				Deployment: &Deployment{
+					Network: NetworkSTN,
+				},
+			},
+			err: nil,
+		}, "valid deployment network config (regtest) should return no errors": {
+			cfg: &Config{
+				Deployment: &Deployment{
+					Network: NetworkRegtest,
+				},
+			},
+			err: nil,
+		}, "deployment network type within other word should fail": {
+			cfg: &Config{
+				Deployment: &Deployment{
+					Network: "btestneth",
+				},
+			},
+			err: errors.New("[deployment.network: value btestneth failed to meet requirements]"),
+		}, "invalid deployment network config should error": {
+			cfg: &Config{
+				Deployment: &Deployment{
+					Network: "blah",
+				},
+			},
+			err: errors.New("[deployment.network: value blah failed to meet requirements]"),
 		},
 	}
 	for name, test := range tests {
