c10e1938ddb9efb6449a512ad619c0d2f18d0bb0
Tíghearnán Carroll <t.carroll@nchain.com>
2021-10-19 11:43:55 +0100

    updated test metric for timestamp

--- service/timestamp_test.go
+++ service/timestamp_test.go
@@ -16,9 +16,8 @@ func TestTimestampService_Nanosecond(t *testing.T) {
 	t.Run("returned timestamp is always now", func(t *testing.T) {
 		prev := svc.Nanosecond()
 		for i := 0; i < 1000; i++ {
-			time.Sleep(4000 * time.Nanosecond)
 			ts := svc.Nanosecond()
-			assert.Greater(t, ts, prev)
+			assert.GreaterOrEqual(t, ts, prev)
 			prev = ts
 		}
 	})
@@ -34,7 +33,7 @@ func TestTimestampService_NowUTC(t *testing.T) {
 		for i := 0; i < 1000; i++ {
 			time.Sleep(4000 * time.Nanosecond)
 			ts := svc.NowUTC()
-			assert.True(t, ts.After(prev))
+			assert.GreaterOrEqual(t, ts.Nanosecond(), prev.Nanosecond())
 			prev = ts
 		}
 	})
